4b3ced57804beeaa9f8a60ce44d3246337b2fcaf
Abu Raihan <aburaihanrahmani@gmail.com>
2025-10-19 10:14:21 -0700

    Cinematic Banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import GridBanner from "@/components/banner/banner";
 import SwipeBanner from "@/components/banner/banner";
+import CinematicBanner from "@/components/banner/banner";
 import Banner from "@/components/banner/banner";
 import Banner1 from "@/components/banner/Banner1";
 import Banner2 from "@/components/banner/Banner2";
@@ -7,11 +8,12 @@ import Banner2 from "@/components/banner/Banner2";
 export default function Home() {
   return (
     <>
-      {/* <Banner /> */}
+      <Banner />
       {/* <Banner1 /> */}
       {/* <Banner2 /> */}
       {/* <GridBanner /> */}
-      <SwipeBanner />
+      {/* <SwipeBanner /> */}
+      {/* <CinematicBanner /> */}
     </>
   );
 }
