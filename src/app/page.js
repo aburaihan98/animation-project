6e71ad0a95bf56126d8847d74009abed447f9731
Abu Raihan <aburaihanrahmani@gmail.com>
2025-10-19 09:18:44 -0700

    Swipe Banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import GridBanner from "@/components/banner/banner";
+import SwipeBanner from "@/components/banner/banner";
 import Banner from "@/components/banner/banner";
 import Banner1 from "@/components/banner/Banner1";
 import Banner2 from "@/components/banner/Banner2";
 
 export default function Home() {
   return (
-    <h1>
+    <>
       {/* <Banner /> */}
       {/* <Banner1 /> */}
       {/* <Banner2 /> */}
-      <GridBanner />
-    </h1>
+      {/* <GridBanner /> */}
+      <SwipeBanner />
+    </>
   );
 }
